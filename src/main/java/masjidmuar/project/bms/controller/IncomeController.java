d3caf01214392117566ed0f0c11ff630f3a02fc6
Razi Claritas <razicharlie@gmail.com>
2024-12-26 00:23:40 +0800

    update cors

--- src/main/java/masjidmuar/project/bms/controller/IncomeController.java
+++ src/main/java/masjidmuar/project/bms/controller/IncomeController.java
@@ -1,16 +1,25 @@
 package masjidmuar.project.bms.controller;
 
-import masjidmuar.project.bms.dto.IncomeDTO;
-import masjidmuar.project.bms.service.IncomeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import masjidmuar.project.bms.dto.IncomeDTO;
+import masjidmuar.project.bms.service.IncomeService;
+
 @RestController
 @RequestMapping("/api/v1/income") 
-@CrossOrigin(origins = {"http://localhost:3000", "https://cmssolution.com.my"})
+@CrossOrigin("https://cmssolution.com.my")
 public class IncomeController {
 
     @Autowired
